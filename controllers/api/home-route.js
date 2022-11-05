3521465869db7c5bb13cf5e0a504abc655ca047d
tillzmh <tillzmh@users.noreply.github.com>
2022-11-05 16:41:37 -0400

    GET the homepage created for page

---
+++ controllers/api/home-route.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+const isPostCreator = require('../utils/isPostCreator');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                }]
+        });
+        const posts = postData.map((post) =>
+            post.get({ plain: true })
+        );
+
+        for (var idx = 0; idx < posts.length; idx++) {
+            posts[idx].dateStringForPost = posts[idx].createdAt.toLocaleDateString();
+        }
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+            pageDescription: 'The Tech Blog'
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login', {
+        pageDescription: 'The Tech Blog'
+    });
+});
