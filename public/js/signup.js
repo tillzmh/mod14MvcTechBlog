53e7cdfb7240dbe1b386397ca4cf4a459e50d7bc
tillzmh <tillzmh@users.noreply.github.com>
2022-11-06 13:02:33 -0500

    sign up page created with password requirments

---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    
+    if (password.length < 6) {
+        alert('Password must be 6 characters or more');
+    }
+    else if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+document
+    .querySelector('#signup-form')
+    .addEventListener('submit', signupFormHandler);
