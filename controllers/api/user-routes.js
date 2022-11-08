4e48bc421cb0ceeb8c0e6099bdd3f28601a28b97
tillzmh <tillzmh@users.noreply.github.com>
2022-11-08 16:54:25 -0500

    user will be able to log in and save the ID in the session so the logged in user is known

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,3 +18,41 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!dbUserData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.loggedInId = dbUserData.dataValues.id;
+
+            res
+                .status(200)
+                .json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
