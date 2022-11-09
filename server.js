99979034531dd008e1e5fb568e936e1b0b339b66
tillzmh <tillzmh@users.noreply.github.com>
2022-11-09 12:33:47 -0500

    installed packages

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-require('dotenv').config();
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// require('dotenv').config();
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-//const helpers = require('./utils/helpers');
+// const routes = require('./controllers');
+// const sequelize = require('./config/connection');
+// //const helpers = require('./utils/helpers');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 15 * 60 * 1000 }, //15 minutes in milliseconds, time before the cookie expires
-    resave: false,
-    rolling: true,  //Reset the cookie Max-Age on every request
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
-};
+// const sess = {
+//     secret: process.env.SESSION_SECRET,
+//     cookie: { maxAge: 15 * 60 * 1000 }, //15 minutes in milliseconds, time before the cookie expires
+//     resave: false,
+//     rolling: true,  //Reset the cookie Max-Age on every request
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize,
+//     }),
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
-//const hbs = exphbs.create({ helpers });
-const hbs = exphbs.create();
+// //const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create();
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
-});
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
+// });
+console.log("Hello, World!")
