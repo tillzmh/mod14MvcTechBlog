455387b5578b97ef30ae1b1f24c3d4aa159e6270
tillzmh <tillzmh@users.noreply.github.com>
2022-11-09 12:52:28 -0500

    tweeks

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-// const path = require('path');
-// const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// require('dotenv').config();
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+require('dotenv').config();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const routes = require('./controllers');
-// const sequelize = require('./config/connection');
-// //const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
-// const app = express();
-// const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//     secret: process.env.SESSION_SECRET,
-//     cookie: { maxAge: 15 * 60 * 1000 }, //15 minutes in milliseconds, time before the cookie expires
-//     resave: false,
-//     rolling: true,  //Reset the cookie Max-Age on every request
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize,
-//     }),
-// };
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// app.use(session(sess));
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 15 * 60 * 1000 }, //15 minutes in milliseconds, time before the cookie expires
+    resave: false,
+    rolling: true,  //Reset the cookie Max-Age on every request
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
 
-// //const hbs = exphbs.create({ helpers });
-// const hbs = exphbs.create();
+app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create();
 
-// app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
-// });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
+});
 console.log("Hello, World!")
